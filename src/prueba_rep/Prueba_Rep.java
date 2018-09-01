545503bbdd0293a4b373c557e642d5be1ef2ccf6
Rcohen90 <39074733+Rcohen90@users.noreply.github.com>
2018-08-31 20:47:08 -0500

    Update Prueba_Rep.java
    Actualización

--- src/prueba_rep/Prueba_Rep.java
+++ src/prueba_rep/Prueba_Rep.java
@@ -15,7 +15,7 @@ public class Prueba_Rep {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Listo!");
+        System.out.println("LTCR TE AMO  ♥");
     }
     
 }
